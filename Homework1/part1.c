df572ce642d376da7c0f5742a98ebc0ceb5e6434
Ömer Faruk <omerfaruk52554@gmail.com>
2025-03-10 22:06:02 +0300

    ödevler eklendi

--- Homework1/part1.c
+++ Homework1/part1.c
@@ -3,38 +3,34 @@ float trueOp(float num1,float num2, char operation);
 float falseOp(float num1,float num2, char operation);
 float main()
 {
-  float num1, num2,error,result,falseResult,errorRate;//float yapıllıcak
+  float num1, num2,error,result,falseResult,errorRate;
+  /*float yapıllıcak*/
   float sumOfErrorRate=0;
   char operation;
-  //1
+  /*1*/
   printf("Enter an operation(Example3+2):");
   scanf("%f %c %f",&num1,&operation,&num2);
-  //true result
+  /*true result*/
   if(operation=='*'){
     result = num1*num2;
-  }
-  if(operation=='/'){
-    result= num1/num2;
-  }
-  if(operation=='+'){
-    result= num1+num2;
-  }
-  if(operation=='-'){
-    result= num1-num2;
-  }
-  //false result
-  if(operation=='*'){
     falseResult = num1+num2;
   }
   if(operation=='/'){
+    if(num2 == 0){
+      printf("Error. you cant divide with 0");
+      return 0;
+    }
+    result= num1/num2;
     falseResult = num1-num2;
   }
   if(operation=='+'){
+    result= num1+num2;
     falseResult = num1-num2;
   }
   if(operation=='-'){
+    result= num1-num2;
     falseResult = num1*num2;
-  } 
+  }
 
   if(result< falseResult){
     error=falseResult-result;
@@ -42,6 +38,9 @@ float main()
   error=result-falseResult;
   }
   errorRate =error/result*100;
+  if (result==0){
+    errorRate=0;
+  }
   sumOfErrorRate+=errorRate;
   printf("Expected Result: %.2f\n",result);
   printf("Experimental Result: %.2f\n",falseResult);
@@ -49,35 +48,30 @@ float main()
   printf("Error Percentage: %.2f%%\n",errorRate);
   printf("Average Error Value: %.2f%%\n",sumOfErrorRate);
 
-  //2
+  /*2*/
   printf("Enter an operation(Example3+2):");
   scanf("%f %c %f",&num1,&operation,&num2);
-  //true result
+  /*true result*/
   if(operation=='*'){
     result = num1*num2;
-  }
-  if(operation=='/'){
-    result= num1/num2;
-  }
-  if(operation=='+'){
-    result= num1+num2;
-  }
-  if(operation=='-'){
-    result= num1-num2;
-  }
-  //false result
-  if(operation=='*'){
     falseResult = num1+num2;
   }
   if(operation=='/'){
+    if(num2 == 0){
+      printf("Error. you cant divide with 0");
+      return 0;
+    }
+    result= num1/num2;
     falseResult = num1-num2;
   }
   if(operation=='+'){
+    result= num1+num2;
     falseResult = num1-num2;
   }
   if(operation=='-'){
+    result= num1-num2;
     falseResult = num1*num2;
-  } 
+  }
 
   if(result< falseResult){
     error=falseResult-result;
@@ -85,6 +79,9 @@ float main()
   error=result-falseResult;
   }
   errorRate =error/result*100;
+  if (result==0){
+    errorRate=0;
+  }
   sumOfErrorRate+=errorRate;
   printf("Expected Result: %.2f\n",result);
   printf("Experimental Result: %.2f\n",falseResult);
@@ -92,35 +89,30 @@ float main()
   printf("Error Percentage: %.2f%%\n",errorRate);
   printf("Average Error Value: %.2f%%\n",sumOfErrorRate/2);
 
-  //3
+  /*3*/
   printf("Enter an operation(Example3+2):");
   scanf("%f %c %f",&num1,&operation,&num2);
-  //true result
+  /*true result*/
   if(operation=='*'){
     result = num1*num2;
-  }
-  if(operation=='/'){
-    result= num1/num2;
-  }
-  if(operation=='+'){
-    result= num1+num2;
-  }
-  if(operation=='-'){
-    result= num1-num2;
-  }
-  //false result
-  if(operation=='*'){
     falseResult = num1+num2;
   }
   if(operation=='/'){
+    if(num2 == 0){
+      printf("Error. you cant divide with 0");
+      return 0;
+    }
+    result= num1/num2;
     falseResult = num1-num2;
   }
   if(operation=='+'){
+    result= num1+num2;
     falseResult = num1-num2;
   }
   if(operation=='-'){
+    result= num1-num2;
     falseResult = num1*num2;
-  }   
+  }
 
   if(result< falseResult){
     error=falseResult-result;
@@ -128,6 +120,9 @@ float main()
   error=result-falseResult;
   }
   errorRate =error/result*100;
+  if (result==0){
+    errorRate=0;
+  }
   sumOfErrorRate+=errorRate;
   printf("Expected Result: %.2f\n",result);
   printf("Experimental Result: %.2f\n",falseResult);
@@ -135,33 +130,28 @@ float main()
   printf("Error Percentage: %.2f%%\n",errorRate);
   printf("Average Error Value: %.2f%%\n",sumOfErrorRate/3);
   
-  //4
+  /*4*/
   printf("Enter an operation(Example3+2):");
   scanf("%f %c %f",&num1,&operation,&num2);
-  //true result
+  /*true result*/
   if(operation=='*'){
     result = num1*num2;
-  }
-  if(operation=='/'){
-    result= num1/num2;
-  }
-  if(operation=='+'){
-    result= num1+num2;
-  }
-  if(operation=='-'){
-    result= num1-num2;
-  }
-  //false result
-  if(operation=='*'){
     falseResult = num1+num2;
   }
   if(operation=='/'){
+    if(num2 == 0){
+      printf("Error. you cant divide with 0");
+      return 0;
+    }
+    result= num1/num2;
     falseResult = num1-num2;
   }
   if(operation=='+'){
+    result= num1+num2;
     falseResult = num1-num2;
   }
   if(operation=='-'){
+    result= num1-num2;
     falseResult = num1*num2;
   }
 
@@ -171,6 +161,9 @@ float main()
   error=result-falseResult;
   }
   errorRate =error/result*100;
+  if (result==0){
+    errorRate=0;
+  }
   sumOfErrorRate+=errorRate;
   printf("Expected Result: %.2f\n",result);
   printf("Experimental Result: %.2f\n",falseResult);
--- Homework1/part2.c
+++ Homework1/part2.c
@@ -4,7 +4,11 @@ int main()
   float r,area;
   printf("r:");
   scanf("%f",&r);
+  if(r<=0){
+    printf("Radius cant be zero or less");
+    return 0;
+  }
   area= 4*3.14*r*r;
-  printf("Area of the sphere:%.2f",area);
+  printf("Area of the sphere:%.2f\n",area);
   return 0;
 }
